70f2d4bc8c5fd09a736b4e2f8300a0dbb37e9ac8
SUPFile Team <team@supfile.local>
2025-12-18 07:28:36 +0100

    Fix: Correction de l'import authMiddleware dans admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middlewares/authMiddleware');
+const { authMiddleware } = require('../middlewares/authMiddleware');
 const adminMiddleware = require('../middlewares/adminMiddleware');
 const adminController = require('../controllers/adminController');
 
